dff2a29340303911439d064020e77cf6418de556
Yushuo Han <yushuo@yushuo-shakudo-mbp.home>
2024-09-26 23:46:59 -0400

    added individual job

---
+++ example-batch-job/example-job.py
@@ -0,0 +1,35 @@
+from minio import Minio
+import time
+from io import BytesIO
+import uuid
+
+time.sleep(10)
+
+MINIO_ENDPOINT = "minio.hyperplane-minio.svc.cluster.local:9000"
+MINIO_ACCESS_KEY = "Czy6vrABD9QwSgHDemVj"
+MINIO_SECRET_KEY = "6YkuLLYe30GMy4SFrNlGAt2r2GTGakjOedJEfBnh"
+
+minio_client = Minio(
+    MINIO_ENDPOINT,
+    access_key=MINIO_ACCESS_KEY,
+    secret_key=MINIO_SECRET_KEY,
+    secure=False
+)
+
+bucket_name = "batch-job-test"
+file_uuid = str(uuid.uuid4())
+object_name = f"{file_uuid}.txt" 
+file_content = f"Hello, MinIO! This is a test string in {object_name}."  
+file_data = BytesIO(file_content.encode('utf-8'))
+
+try:
+    minio_client.put_object(
+        bucket_name=bucket_name,
+        object_name=object_name,
+        data=file_data,
+        length=len(file_content), 
+        content_type="text/plain"
+    )
+    print(f"Text file '{object_name}' successfully uploaded to bucket '{bucket_name}'.")
+except S3Error as e:
+    raise Exception(f"Failed to upload file: {str(e)}")
