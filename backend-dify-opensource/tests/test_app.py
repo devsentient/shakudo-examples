9ac95056970de747c6cd3ddce69a1869b73aa0c3
Tirth Patel <tirth@shakudo.io>
2024-11-20 15:31:53 +0000

    Fix return type - do not return json output as unable to parse in dify workflow

--- backend-dify-opensource/tests/test_app.py
+++ backend-dify-opensource/tests/test_app.py
@@ -14,35 +14,35 @@
 # print(response.json())
 
 
-# import requests
-
-# url = "http://0.0.0.0:8000/generateSQL"
-
-# # Pass data as JSON body
-# data = {
-#     "prompt": "what are top 5 country in climate change metadata table?",
-#     "schema": "climate",
-#     "tables": ["climate_change_data_metadata"]
-# }
-
-# response = requests.post(url, json=data)  # Use POST and send JSON body
-# print(response.json())
-
-
 import requests
 
-# Define the URL of your endpoint
-url = "http://0.0.0.0:8000/validateAndExecuteSQL"
+url = "http://hyperplane-service-016559.hyperplane-pipelines.svc.cluster.local:8787/generateSQL"
 
-# Define the SQL code to validate and execute
-params = {
-    "sqlCode": "SELECT COUNT(*) FROM climate.climate_change_data_metadata;"
+# # Pass data as JSON body
+data = {
+    "prompt": "what are top 5 country in climate change metadata table?",
+    "schema": "climate",
+    "tables": ["climate_change_data_metadata"]
 }
 
-# Send a GET request with the SQL code as a query parameter
-response = requests.get(url, params=params)
-
-# Print the response from the server
+response = requests.post(url, json=data)  # Use POST and send JSON body
 print(response.json())
+
+
+# import requests
+
+# # Define the URL of your endpoint
+# url = "http://0.0.0.0:8000/validateAndExecuteSQL"
+
+# # Define the SQL code to validate and execute
+# params = {
+#     "sqlCode": "SELECT COUNT(*) FROM climate.climate_change_data_metadata;"
+# }
+
+# # Send a GET request with the SQL code as a query parameter
+# response = requests.get(url, params=params)
+
+# # Print the response from the server
+# print(response.json())
 
 
