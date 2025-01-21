d3aa5def6836ef82c48746a0f6ad96a6a6de485d
Yushuo Han <yushuo@shakudo.io>
2025-01-21 15:36:21 +0000

    change port

--- graph-rag/run.sh
+++ graph-rag/run.sh
@@ -3,4 +3,4 @@ cd graph-rag
 apt-get update -y && apt-get upgrade -y
 
 pip install -r requirements.txt
-uvicorn app:app --workers 1 --port 8000 --host 0.0.0.0
+uvicorn app:app --workers 1 --port 8787 --host 0.0.0.0
