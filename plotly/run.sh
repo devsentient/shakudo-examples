19875ddfeda6151d05988bcae9a149c6d1fb7a90
Elliott <elliott@shakudo.io>
2025-05-20 17:41:21 +0000

    set up virtualenv to support lower Python version

--- plotly/run.sh
+++ plotly/run.sh
@@ -1,2 +1,11 @@
+# Create a virtual environment called plotly-venv
+python3 -m venv plotly-venv
+
+# Activate the virtual environment
+source plotly-venv/bin/activate
+
+# Install dependencies
 pip install -r plotly/requirements.txt
+
+# Run the Dash app
 python plotly/mini-plot-server.py
